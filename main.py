812eff48b980ea1ca9573f6b1800d0ff87bcd442
Eric Yang <erichyang11@gmail.com>
2021-09-12 00:30:56 -0500

    main menu music

--- main.py
+++ main.py
@@ -65,8 +65,7 @@ def main():
 
     # main, challenge, free
     game_state = 'main'
-    pygame.mixer.music.load('./assets/twinkle_song.mp3')
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(7).play(pygame.mixer.Sound('./assets/audio/twinkle_song.mp3'))
     cursor_on = False
     main_bg = pygame.image.load("./assets/main_menu_bg.png")
     game_bg = pygame.image.load("./assets/game_bg.png")
@@ -85,11 +84,11 @@ def main():
                     button_3 = pygame.Rect(WIDTH / 2 - 75, HEIGHT / 2 + 225, 150, 75)
                     if button_1.collidepoint((mx, my)):
                         game_state = 'challenge'
-                        pygame.mixer.music.stop()
+                        pygame.mixer.stop()
                         continue
                     if button_2.collidepoint((mx, my)):
                         game_state = 'free'
-                        pygame.mixer.music.stop()
+                        pygame.mixer.stop()
                         continue
                     if button_3.collidepoint((mx, my)):
                         run = False
@@ -100,13 +99,13 @@ def main():
                     button_4 = pygame.Rect(20, HEIGHT - 70, 150, 50)
                     if button_4.collidepoint((mx, my)):
                         game_state = 'main'
+                        # background music in main_menu
+                        pygame.mixer.Channel(7).play(pygame.mixer.Sound('./assets/audio/twinkle_song.mp3'))
                         continue
 
         if game_state == 'main':
             screen.blit(main_bg, main_bg.get_rect())
-            # background music in main_menu
-            pygame.mixer.music.load('./assets/twinkle_song.mp3')
-            pygame.mixer.music.play(-1)
+
         elif game_state == 'challenge':
             screen.blit(game_bg, game_bg.get_rect())
             screen.blit(header, ((WIDTH-header.get_width())/2, 0))
