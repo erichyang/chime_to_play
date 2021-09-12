ef79a6eb25b769a847b8064246a79777deed1afd
Mengting Teng <Mengting.teng@gmail.com>
2021-09-11 20:54:10 -0500

    created level notes

--- audio.py
+++ audio.py
@@ -2,6 +2,12 @@ import pygame
 
 pygame.mixer.init(44100, -16, 2, 2048)
 
+# section 3 is faster
+song_note = [('e6', 'e6', 'e6', 'd6', 'e6', 'g6', 'e6', 'd6', 'd6', 'd6', 'c6', 'd6', 'g6'),
+             ('c6', 'd6', 'g6', 'e6', 'g6', 'a6', 'g6', 'a6', 'e6', 'd6', 'e6', 'd6', 'c6'),
+             ('e6', 'e6', 'g6', 'e6', 'g6', 'e6', 'g6', 'd6', 'd6', 'e6', 'd6', 'e6', 'd6',
+              'e6', 'd6', 'g6', 'd6', 'e6', 'g6', 'e6', 'e6', 'ge', 'd6', 'c6'),
+             ('c6', 'd6', 'g6', 'e6', 'g6', 'a6', 'g6', 'a6', 'e6', 'd6', 'e6', 'd6', 'c6')]
 
 def c6():
     pygame.mixer.music.load("./assets/audio/C6.mp3")
