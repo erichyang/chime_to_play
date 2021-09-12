a4f9a0041ad94b26109c30048a08ab9ae47738d5
Eric Yang <erichyang11@gmail.com>
2021-09-11 22:43:54 -0500

    change to multi-channel sounds

--- audio.py
+++ audio.py
@@ -9,29 +9,25 @@ song_note = [('e6', 'e6', 'e6', 'd6', 'e6', 'g6', 'e6', 'd6', 'd6', 'd6', 'c6', 
               'e6', 'd6', 'g6', 'd6', 'e6', 'g6', 'e6', 'e6', 'ge', 'd6', 'c6'),
              ('c6', 'd6', 'g6', 'e6', 'g6', 'a6', 'g6', 'a6', 'e6', 'd6', 'e6', 'd6', 'c6')]
 
+
 def c6():
-    pygame.mixer.music.load("./assets/audio/C6.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(0).play(pygame.mixer.Sound(r"./assets/audio/C6.mp3"))
 
 
 def d6():
-    pygame.mixer.music.load(r"./assets/audio/D6.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(1).play(pygame.mixer.Sound(r"./assets/audio/D6.mp3"))
 
 
 def e6():
-    pygame.mixer.music.load(r"./assets/audio/E6.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(2).play(pygame.mixer.Sound(r"./assets/audio/E6.mp3"))
 
 
 def g6():
-    pygame.mixer.music.load(r"./assets/audio/g6.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(3).play(pygame.mixer.Sound(r"./assets/audio/G6.mp3"))
 
 
 def a6():
-    pygame.mixer.music.load(r"./assets/audio/a6.mp3")
-    pygame.mixer.music.play(-1)
+    pygame.mixer.Channel(4).play(pygame.mixer.Sound(r"./assets/audio/A6.mp3"))
 
 
 # from playsound import playsound
